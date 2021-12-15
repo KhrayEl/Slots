d05db45efc1876bcf4243215332b6951454a95a2
Dinder <khrayel@gmail.com>
2021-12-15 15:22:12 +0300

    reels no longer accumulate extra textviews

--- app/src/main/java/com/khrayel/slots/SlotsClickHandler.java
+++ app/src/main/java/com/khrayel/slots/SlotsClickHandler.java
@@ -1,7 +1,9 @@
 package com.khrayel.slots;
 
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -65,6 +67,17 @@ public class SlotsClickHandler
 //                tv2.setTextColor(v.getResources().getColor(R.color.transparent));
 //                tv3.setTextColor(v.getResources().getColor(R.color.transparent));
 
+                //                handler.postDelayed(new Runnable()
+//                    {
+//                        public void run ()
+//                            {
+//                                for (int i = 0; i < linearLayout_reel.getChildCount()  -children_in_reel; i++)
+//                                    {
+//                                        linearLayout_reel.removeViewAt(i);
+//                                    }
+//                            }
+//            },reel_anim_delay*20);
+
 
                 Button btn_spin = parent.findViewById(R.id.slots_button_spin);
                 Button btn_plus = parent.findViewById(R.id.slots_button_bet_plus);
@@ -87,21 +100,11 @@ public class SlotsClickHandler
                 //View view = parent.findViewById(R.id.slots_text_reel1);
 
 
-                StartReel(parent.findViewById(R.id.slots_layout_scroll_1), slotsViewModel.getRoll1(), handler, 1);
-                StartReel(parent.findViewById(R.id.slots_layout_scroll_2), slotsViewModel.getRoll2(), handler, 2);
-                StartReel(parent.findViewById(R.id.slots_layout_scroll_3), slotsViewModel.getRoll3(), handler, 3);
+                StartReel(parent.findViewById(R.id.slots_layout_scroll_1), parent.findViewById(R.id.slots_layout_reel_1), slotsViewModel.getRoll1(), handler, 1);
+                StartReel(parent.findViewById(R.id.slots_layout_scroll_2), parent.findViewById(R.id.slots_layout_reel_2),slotsViewModel.getRoll2(), handler, 2);
+                StartReel(parent.findViewById(R.id.slots_layout_scroll_3), parent.findViewById(R.id.slots_layout_reel_3),slotsViewModel.getRoll3(), handler, 3);
 
 
-//                handler.postDelayed(new Runnable()
-//                    {
-//                        public void run ()
-//                            {
-//                                for (int i = 0; i < linearLayout_reel.getChildCount()  -children_in_reel; i++)
-//                                    {
-//                                        linearLayout_reel.removeViewAt(i);
-//                                    }
-//                            }
-//            },reel_anim_delay*20);
                 //  focusOnView((ScrollView) scroll, linearLayout_reel.getChildAt(2), handler, 0);
 
 
@@ -124,8 +127,10 @@ public class SlotsClickHandler
 
             }
 
-        private static void AddChildrenToReel (ViewGroup parent, int number_of_children_to_add,String rolled_value)
+        private static void AddChildrenToReel (ViewGroup parent, int number_of_children_to_add, String rolled_value, Handler handler)
             {
+
+
                 for (int i = 0; i < number_of_children_to_add; i++)
                     {
                         TextView tv = new TextView(parent.getContext());
@@ -151,6 +156,26 @@ public class SlotsClickHandler
 
         private static void focusOnView (final ScrollView scroll, final View view, Handler handler, int scrolling_duration)
             {
+                // Get duration scale from the global settings.
+                float durationScale = Settings.Global.getFloat(scroll.getContext().getContentResolver(),
+                        Settings.Global.ANIMATOR_DURATION_SCALE, 0);
+
+// If global duration scale is not 1 (default), try to override it
+// for the current application.
+                if (durationScale != 1)
+                    {
+                        try
+                            {
+                                ValueAnimator.class.getMethod("setDurationScale", float.class).invoke(null, 1f);
+                                durationScale = 1f;
+                            } catch (Throwable t)
+                            {
+                                // It means something bad happened, and animations are still
+                                // altered by the global settings. You should warn the user and
+                                // exit application.
+                            }
+                    }
+
                 handler.post(new Runnable()
                     {
                         @Override
@@ -160,7 +185,10 @@ public class SlotsClickHandler
                                 int vBottom = view.getBottom();
                                 int sHeight = scroll.getBottom();
 //                                scroll.smoothScrollBy(0, ((vTop + vBottom - sHeight) / 2));
-                                ObjectAnimator.ofInt(scroll, "scrollY", (vTop + vBottom - sHeight) / 2).setDuration(scrolling_duration).start();
+                                ObjectAnimator.ofInt(scroll, "scrollY", (vTop + vBottom - sHeight) / 2)
+                                        .setDuration(scrolling_duration)
+                                        .start();
+
                             }
                     });
             }
@@ -177,26 +205,46 @@ public class SlotsClickHandler
                 slotsViewModel.Restart();
             }
 
-        private static void StartReel (ScrollView scrollView, String rolled_value, Handler handler, int reel_multiplier)
+        private static void StartReel (ScrollView scrollView,LinearLayout linearLayout_reel , String rolled_value, Handler handler, int reel_multiplier)
             {
-                LinearLayout linearLayout_reel = (LinearLayout) scrollView.getChildAt(0);
+                //LinearLayout linearLayout_reel = (LinearLayout) scrollView.getChildAt(0);
                 int childs_to_add = 100;
-                int children_in_reel = 5;
+
                 int reel_anim_delay = 50;
 
+                int children_in_reel = linearLayout_reel.getChildCount();
+                for (int i = childs_to_add * reel_multiplier; i >0 /*linearLayout_reel.getChildCount() - 5*/
+                         && children_in_reel > 5; i--)
+                    {
+                        try
+                            {
+                                linearLayout_reel.removeViewAt(i);
+                            } catch (Exception e)
+                            {
+                                e.printStackTrace();
+                            }
 
-                AddChildrenToReel(linearLayout_reel, childs_to_add * reel_multiplier, rolled_value);
-
-                int index_of_child_to_focus = linearLayout_reel.getChildCount() - 3;
-
-//                focusOnView((ScrollView) scroll, linearLayout_reel.getChildAt(2), handler, 0);
+                    }
+                children_in_reel = linearLayout_reel.getChildCount();
+                focusOnView((ScrollView) scrollView, linearLayout_reel.getChildAt(children_in_reel - 3), handler, 0);
 //                scroll.scrollTo(0, (
 //                        (linearLayout_reel.getChildAt(index_of_child_to_focus).getTop() +
 //                                linearLayout_reel.getChildAt(index_of_child_to_focus).getBottom() -
 //                                scroll.getBottom())
 //                 / 2));
+                AddChildrenToReel(linearLayout_reel, childs_to_add * reel_multiplier, rolled_value, handler);
+
+                int index_of_child_to_focus = linearLayout_reel.getChildCount() - 3;
+
+//
                 focusOnView((ScrollView) scrollView, linearLayout_reel.getChildAt(index_of_child_to_focus), handler, reel_anim_delay * 10 * reel_multiplier);
+                ;
+//                handler.postDelayed(new Runnable()
+//                    {
+//                        public void run ()
+//                            {
 
-
+//                            }
+//                    },0);
             }
     }
